92982ccbec21b9ca8863cfecc3686c12f2e4eedf
Alex Taghva <alextaghva@gmail.com>
2010-10-26 10:14:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Istim::Application.config.secret_token = 'c444a5dd751a2db47f177da8ed84bb91e84c8b977f47e06fef461ce83e993647935ec3df6875bbc6e0a789b3828a2e9ac44626245499488b123655276dbdca8a'
